7bebcd82c36ff5079c2f544c543ef94170d1bfb7
Kent Rancourt <kent.rancourt@microsoft.com>
2018-12-12 15:41:21 -0500

    improve activator

--- pkg/deployments/activator/app.go
+++ pkg/deployments/activator/app.go
@@ -1,8 +1,14 @@
 package activator
 
+import (
+	"net/http/httputil"
+	"net/url"
+)
+
 type app struct {
-	namespace      string
-	serviceName    string
-	deploymentName string
-	targetHost     string
+	namespace           string
+	serviceName         string
+	deploymentName      string
+	targetURL           *url.URL
+	proxyRequestHandler *httputil.ReverseProxy
 }
