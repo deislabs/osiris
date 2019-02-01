78780f09ba1e2456a6da5a56c512bc6c0dc672f4
Kent Rancourt <kent.rancourt@microsoft.com>
2019-02-01 10:03:05 -0500

    update zeroscaler algorithm for scale-to-zero decision

--- pkg/deployments/zeroscaler/metrics_collector.go
+++ pkg/deployments/zeroscaler/metrics_collector.go
@@ -31,7 +31,8 @@ type metricsCollector struct {
 	selector             labels.Selector
 	metricsCheckInterval time.Duration
 	podsInformer         cache.SharedIndexInformer
-	appPods              map[string]*corev1.Pod
+	currentAppPods       map[string]*corev1.Pod
+	allAppPodStats       map[string]*podStats
 	appPodsLock          sync.Mutex
 	httpClient           *http.Client
 	cancelFunc           func()
@@ -56,7 +57,8 @@ func newMetricsCollector(
 			nil,
 			selector,
 		),
-		appPods: map[string]*corev1.Pod{},
+		currentAppPods: map[string]*corev1.Pod{},
+		allAppPodStats: map[string]*podStats{},
 		// A very aggressive timeout. When collecting metrics, we want to do it very
 		// quickly to minimize the possibility that some pods we've checked on have
 		// served requests while we've been checking on OTHER pods.
@@ -103,70 +105,121 @@ func (m *metricsCollector) syncAppPod(obj interface{}) {
 	m.appPodsLock.Lock()
 	defer m.appPodsLock.Unlock()
 	pod := obj.(*corev1.Pod)
-	m.appPods[pod.Name] = pod
+	m.currentAppPods[pod.Name] = pod
+	if _, ok := m.allAppPodStats[pod.Name]; !ok {
+		m.allAppPodStats[pod.Name] = &podStats{}
+	}
 }
 
 func (m *metricsCollector) syncDeletedAppPod(obj interface{}) {
 	m.appPodsLock.Lock()
 	defer m.appPodsLock.Unlock()
 	pod := obj.(*corev1.Pod)
-	delete(m.appPods, pod.Name)
+	delete(m.currentAppPods, pod.Name)
+	now := time.Now()
+	if ps, ok := m.allAppPodStats[pod.Name]; ok {
+		ps.podDeletedTime = &now
+	}
 }
 
 func (m *metricsCollector) collectMetrics(ctx context.Context) {
-	connectionStatsByProxy := map[string]metrics.ProxyConnectionStats{}
-	var lastOpenedConnectionCount uint64
 	ticker := time.NewTicker(m.metricsCheckInterval)
 	defer ticker.Stop()
+	var periodStartTime, periodEndTime *time.Time
 	for {
 		select {
 		case <-ticker.C:
-			m.appPodsLock.Lock()
-			var mustNotDecide bool
-			var scrapeWG sync.WaitGroup
-			// An aggressively small timeout. We make the decision fast or not at
-			// all.
-			timer := time.NewTimer(3 * time.Second)
-			for _, pod := range m.appPods {
-				podMetricsPort, ok := getMetricsPort(pod)
-				if !ok {
-					mustNotDecide = true
-					continue
-				}
-				url := fmt.Sprintf(
-					"http://%s:%d/metrics",
-					pod.Status.PodIP,
-					podMetricsPort,
-				)
-				scrapeWG.Add(1)
-				go func() {
-					defer scrapeWG.Done()
-					// Get the results
-					pcs, ok := m.scrape(url)
+			periodStartTime = periodEndTime
+			now := time.Now()
+			periodEndTime = &now
+			// Wrap in a function so we can more easily defer some cleanup that has
+			// to be executed regardless of which condition causes us to continue to
+			// the next iteration of the loop.
+			func() {
+				m.appPodsLock.Lock()
+				defer m.appPodsLock.Unlock()
+				// An aggressively small timeout. We make the decision fast or not at
+				// all.
+				timer := time.NewTimer(3 * time.Second)
+				defer timer.Stop()
+				var timedOut bool
+				// Get metrics for all of the deployment's CURRENT pods.
+				var scrapeWG sync.WaitGroup
+				for _, pod := range m.currentAppPods {
+					podMetricsPort, ok := getMetricsPort(pod)
 					if !ok {
-						mustNotDecide = true
+						continue
 					}
-					connectionStatsByProxy[pcs.ProxyID] = pcs
-				}()
-			}
-			m.appPodsLock.Unlock()
-			scrapeWG.Wait()
-			var openedConnectionCount uint64
-			for _, pcs := range connectionStatsByProxy {
-				openedConnectionCount += pcs.ConnectionsOpened
-			}
-			select {
-			case <-timer.C:
-				mustNotDecide = true
-			case <-ctx.Done():
-				return
-			default:
-			}
-			timer.Stop()
-			if !mustNotDecide && openedConnectionCount == lastOpenedConnectionCount {
-				m.scaleToZero()
-			}
-			lastOpenedConnectionCount = openedConnectionCount
+					url := fmt.Sprintf(
+						"http://%s:%d/metrics",
+						pod.Status.PodIP,
+						podMetricsPort,
+					)
+					scrapeWG.Add(1)
+					go func(podName string) {
+						defer scrapeWG.Done()
+						// Get the results
+						pcs, ok := m.scrape(url)
+						if ok {
+							ps := m.allAppPodStats[podName]
+							ps.prevStatTime = ps.recentStatTime
+							ps.prevStats = ps.recentStats
+							ps.recentStatTime = periodEndTime
+							ps.recentStats = &pcs
+						}
+					}(pod.Name)
+				}
+				// Wait until we're done checking all pods.
+				scrapeWG.Wait()
+				// If this is our first check, we're done because we will have no
+				// previous stats to compare recent stats to.
+				if periodStartTime == nil {
+					return
+				}
+				// Now iterate over stats for ALL of the deployment's pods-- this may
+				// include pods that died since the last check-- their stats should
+				// still count, but since we won't have stats for those, we'll have to
+				// err on the side of caution and assume activity in such cases.
+				var foundActivity, assumedActivity bool
+				for podName, ps := range m.allAppPodStats {
+					if ps.podDeletedTime != nil &&
+						ps.podDeletedTime.Before(*periodStartTime) {
+						// This pod was deleted before the period we'red concerned with, so
+						// stats from this pod are not relevant anymore or ever again.
+						delete(m.allAppPodStats, podName)
+						continue
+					}
+					// If we already assumed some activity or found some, fast forward to
+					// the next pod. We don't simply break out of the whole loop because
+					// the logic above removes stats for pods that are no longer relevant
+					// and we still want that to happen promptly.
+					if assumedActivity || foundActivity {
+						continue
+					}
+					// nolint: lll
+					if (ps.prevStatTime == nil || ps.recentStatTime == nil) || // We cannot make meaningful comparisons for this pod
+						ps.recentStatTime.Before(*periodEndTime) || // We don't have up-to-date stats for this pod
+						ps.recentStats.ProxyID != ps.prevStats.ProxyID { // The pod's metrics-collecting proxy sidecar died and was replaced
+						assumedActivity = true
+						continue
+					}
+					// nolint: lll
+					if ps.recentStats.ConnectionsOpened > ps.prevStats.ConnectionsOpened || // New connections have been opened
+						ps.recentStats.ConnectionsClosed > ps.prevStats.ConnectionsClosed || // Opened connections were closed
+						ps.recentStats.ConnectionsOpened > ps.recentStats.ConnectionsClosed { // Some connections remain open
+						foundActivity = true
+						continue
+					}
+				}
+				select {
+				case <-timer.C:
+					timedOut = true
+				default:
+				}
+				if !(timedOut || foundActivity || assumedActivity) {
+					m.scaleToZero()
+				}
+			}()
 		case <-ctx.Done():
 			return
 		}
