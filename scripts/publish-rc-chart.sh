caa6512f42d2a6e6fe4e36ddd3c38e0342ea42e3
Kent Rancourt <kent.rancourt@microsoft.com>
2019-11-25 12:10:47 -0500

    improve how chart publishing works

--- scripts/publish-rc-chart.sh
+++ scripts/publish-rc-chart.sh
@@ -13,12 +13,10 @@ cp -r -L chart/osiris /tmp/osiris-edge
 cd /tmp/osiris-edge
 
 sed -i "s/^name:.*/name: osiris-edge/g" Chart.yaml
-sed -i "s/^version:.*/version: 0.0.1-$DATE-$GIT_VERSION/g" Chart.yaml
-sed -i "s/^appVersion:.*/appVersion: $GIT_VERSION/g" Chart.yaml
 sed -i "s/^  tag:.*/  tag: $GIT_VERSION/g" values.yaml
 sed -i "s/^  pullPolicy:.*/  pullPolicy: IfNotPresent/g" values.yaml
 
 helm dep build .
-helm package .
+helm package --version 0.0.1-$DATE-$GIT_VERSION --app-version $GIT_VERSION .
 
 az acr helm push -n osiris osiris-edge-0.0.1-$DATE-$GIT_VERSION.tgz
